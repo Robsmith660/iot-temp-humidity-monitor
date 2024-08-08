927e8c630671c5920f9346cd73d744822663ac2d
Robert Smith <r.smith25@universityofGalway.ie>
2024-08-08 03:44:44 +0100

    Update .ebextensions configuration

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
